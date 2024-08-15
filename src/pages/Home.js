412d41181572f51831601bc9ac5d6fc495cb1571
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2024-08-15 19:38:21 +0530

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Home.scss"
+// import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+   <div className='home'>
+    {/* <Sidebar/>
+    <div className='home-container'>
+        Container
+    </div> */}
+    <h1>Home</h1>
+   </div>
+  )
+}
+
+export default Home
