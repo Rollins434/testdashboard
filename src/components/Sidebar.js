e9e7d43444131a92d2bca1e0ceaa175f2e741530
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2024-08-20 18:11:04 +0530

    sidebar shrink

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.scss";
 import { Link, NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const [isShrink, setIsShrink] = useState(false);
+
+  function handleSidebar(){
+    setIsShrink(!isShrink)
+  }
+
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{ width: isShrink? "80px" : "300px" }}>
+      
       <div className="sidebar-content">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/">
+            {!isShrink ? "Home" : "H" }
+            </NavLink>
           </li>
           <li>
-            <Link to="/forecasting">Forecasting</Link>
+            <NavLink to="/forecasting">{!isShrink ? "Forecasting" : "F"}</NavLink>
           </li>
         </ul>
       </div>
+      <button onClick={handleSidebar} >&#10594;</button>
     </div>
   );
 };
